c25cc70c15e43c5a3ee993be4a9d70acb0258ca1
Florian Richter <florian.richter.de@gmail.com>
2020-07-31 10:36:19 +0200

    Add dedicated test for skill_check

---
+++ test/test_skill_check.py
@@ -0,0 +1,141 @@
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+from bot import skill_check
+
+
+class MockAuthor:
+    def __init__(self, name):
+        self.mention = "@{}".format(name)
+
+
+class TestDiceRoll(TestCase):
+    def test_parse(self):
+        self.assertIsNotNone(skill_check.parse("13"))
+        self.assertIsNotNone(skill_check.parse("1,12,18"))
+        self.assertIsNotNone(skill_check.parse("8 19 1400"))
+        self.assertIsNotNone(skill_check.parse("2 2, 2, 2,2,2"))
+
+    def test_parse_with_fw(self):
+        self.assertIsNotNone(skill_check.parse("13@2"))
+        self.assertIsNotNone(skill_check.parse("1,12,18@18"))
+        self.assertIsNotNone(skill_check.parse("8 19 1400@0"))
+        self.assertIsNotNone(skill_check.parse("2 2, 2, 2,2,2@1400"))
+
+    def test_parse_with_prefix(self):
+        self.assertIsNotNone(skill_check.parse("!13 1@2"))
+        self.assertIsNotNone(skill_check.parse("! 1,12,18@18"))
+
+        self.assertIsNone(skill_check.parse("!!13 1@2"))
+        self.assertIsNone(skill_check.parse("!  1 13@0"))
+        self.assertIsNone(skill_check.parse("!?4"))
+        self.assertIsNone(skill_check.parse("#2,2,2@2"))
+
+    def test_parse_with_large_numbers(self):
+        self.assertIsNotNone(skill_check.parse("1337@1337"))
+        self.assertIsNotNone(skill_check.parse("100000 100000 1000000 @ 1000000"))
+        self.assertIsNotNone(skill_check.parse("9999999@88888888"))
+
+    def test_parse_with_modifiers(self):
+        self.assertIsNotNone(skill_check.parse("12 12 12@8+3"))
+        self.assertIsNotNone(skill_check.parse("8,9,10-4"))
+        self.assertIsNotNone(skill_check.parse("!4 5 16 18@17-4"))
+        self.assertIsNotNone(skill_check.parse("!1 + 1 - 4"))
+
+    def test_parse_with_comment(self):
+        self.assertIsNotNone(skill_check.parse("12 test"))
+        self.assertIsNotNone(skill_check.parse("!12,12@8+3 this is a comment"))
+        self.assertIsNotNone(skill_check.parse("!12,12@8-6 lolololol"))
+        self.assertIsNotNone(skill_check.parse("20 + 1 - 4 can I put anything here? 🤔"))
+
+    def test_parse_with_other_commands(self):
+        self.assertIsNone(skill_check.parse("d3"))
+        self.assertIsNone(skill_check.parse("note:foobar"))
+        self.assertIsNone(skill_check.parse("SUMMON"))
+        self.assertIsNone(skill_check.parse("BEGONE"))
+        self.assertIsNone(skill_check.parse("DIE"))
+
+        # Careful, these are not None. Execution order matters!
+        # self.assertIsNone(skill_check.parse("!3w6"))
+        # self.assertIsNone(skill_check.parse("12d12+2"))
+
+    def test_parse_results(self):
+        parsed = skill_check.parse("11,13,13@7-2 Sinnesschärfe")
+        self.assertEqual(parsed.group("attr"), "11,13,13")
+        self.assertEqual(parsed.group("FW"), "7")
+        self.assertEqual(parsed.group("add"), None)
+        self.assertEqual(parsed.group("sub"), "2")
+        self.assertEqual(parsed.group("comment"), "Sinnesschärfe")
+
+        parsed = skill_check.parse("1")
+        self.assertEqual(parsed.group("attr"), "1")
+        self.assertEqual(parsed.group("FW"), None)
+        self.assertEqual(parsed.group("add"), None)
+        self.assertEqual(parsed.group("sub"), None)
+        self.assertEqual(parsed.group("comment"), "")
+
+        parsed = skill_check.parse("!111 1337 42 1 1 @ 27 +1 - 2 🎉")
+        self.assertEqual(parsed.group("attr"), "111 1337 42 1 1 ")
+        self.assertEqual(parsed.group("FW"), "27")
+        self.assertEqual(parsed.group("add"), "1")
+        self.assertEqual(parsed.group("sub"), "2")
+        self.assertEqual(parsed.group("comment"), "🎉")
+
+    @patch("random.randint", new_callable=MagicMock())
+    def test_response(self, mock_randint: MagicMock):
+        mock_randint.return_value = 8
+        result = skill_check.create_response(
+            skill_check.parse("4"), MockAuthor("TestUser")
+        )
+        self.assertEqual(result, "@TestUser \n8 ===> -4")
+
+        result = skill_check.create_response(
+            skill_check.parse("12, 13, 6"), MockAuthor("TestUser")
+        )
+        self.assertEqual(result, "@TestUser \n8, 8, 8 ===> -2")
+
+    @patch("random.randint", new_callable=MagicMock())
+    def test_response_with_fw(self, mock_randint: MagicMock):
+        mock_randint.return_value = 9
+        result = skill_check.create_response(
+            skill_check.parse("10,7@2"), MockAuthor("TestUser")
+        )
+        self.assertEqual(result, "@TestUser \n9, 9 ===> -2\n(2 - 2 = 0 FP) QS: 1")
+
+        result = skill_check.create_response(
+            skill_check.parse("!6,9,6@3"), MockAuthor("TestUser")
+        )
+        self.assertEqual(
+            result, "@TestUser \n9, 9, 9 ===> -6\n(3 - 6 = -3 FP) QS: 0 FAIL"
+        )
+
+    @patch("random.randint", new_callable=MagicMock())
+    def test_response_with_modifier(self, mock_randint: MagicMock):
+        mock_randint.return_value = 10
+        result = skill_check.create_response(
+            skill_check.parse("10,7@2+3"), MockAuthor("TestUser")
+        )
+        self.assertEqual(result, "@TestUser \n10, 10 ===> 0\n(2 - 0 = 2 FP) QS: 1")
+
+        result = skill_check.create_response(
+            skill_check.parse("!12,10,14@7-1"), MockAuthor("TestUser")
+        )
+        self.assertEqual(result, "@TestUser \n10, 10, 10 ===> -1\n(7 - 1 = 6 FP) QS: 2")
+
+        result = skill_check.create_response(
+            skill_check.parse("!12,12,12@12 - 1"), MockAuthor("TestUser")
+        )
+        self.assertEqual(
+            result, "@TestUser \n10, 10, 10 ===> 0\n(12 - 0 = 12 FP) QS: 4"
+        )
+
+    @patch("random.randint", new_callable=MagicMock())
+    def test_response_with_comment(self, mock_randint: MagicMock):
+        mock_randint.return_value = 10
+        result = skill_check.create_response(
+            skill_check.parse("! 12,12,12 @ 12 + 2 Sinnesschärfe"),
+            MockAuthor("TestUser"),
+        )
+        self.assertEqual(
+            result, "@TestUser Sinnesschärfe\n10, 10, 10 ===> 0\n(12 - 0 = 12 FP) QS: 4"
+        )
