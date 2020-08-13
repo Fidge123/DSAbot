50fd2b7fb8e362d3e38b542b6d6a6e2a13718781
Florian Richter <florian.richter.de@gmail.com>
2020-08-13 10:31:10 +0200

    Sort notes by key

--- bot/note.py
+++ bot/note.py
@@ -12,9 +12,10 @@ def on_load():
 
 
 def notes_to_str():
-    lk = max(map(len, number_notes.keys()))
+    sorted_notes = sorted(number_notes.items(), key=lambda x: x[0])
+    lk = max(map(len, number_notes.keys())) + 1
     lv = max(map(lambda x: len(str(x)), number_notes.values()))
-    return "\n".join([f"{k:<{lk}}: {v:>{lv}}" for k, v in number_notes.items()])
+    return "\n".join([f"{k:<{lk}}: {v:>{lv}}" for k, v in sorted_notes])
 
 
 def create_note(note_id, value, user):
