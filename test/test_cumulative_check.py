8edc4a5b7a0368c3f9f4cf9ccea498303db13d88
Florian Richter <florian.richter.de@gmail.com>
2020-08-18 09:55:20 +0200

    Add more tests

--- test/test_cumulative_check.py
+++ test/test_cumulative_check.py
@@ -44,28 +44,92 @@ class TestCumulativeCheck(TestCase):
         with self.assertRaises(ValueError):
             CumulativeCheck("13,13,13+1", MockAuthor("TestUser"))
 
-    # @patch("random.randint", new_callable=MagicMock())
-    # def test_end2end_botch(self, mock_randint: MagicMock):
-    #     mock_randint.return_value = 12
-    #     cc = CumulativeCheck(
-    #         "!Sammelprobe 5x3Tage 12 14 10 @ 3", MockAuthor("TestUser")
-    #     )
-    #     self.assertEqual(cc.data["attributes"], [17, 16, 10])
-    #     self.assertEqual(cc.data["EAV"], [17, 16, 10])
-    #     self.assertEqual(cc.data["modifier"], 0)
-    #     self.assertEqual(cc.data["SR"], 10)
-    #     self.assertEqual(cc.data["comment"], "")
-    #     self.assertEqual(cc.data["author"], "@TestUser")
-    #     self.assertEqual(cc.data["rolls"].rolls, [20, 20, 20])
-    #     self.assertEqual(cc.data["rolls"].critical_success, False)
-    #     self.assertEqual(cc.data["rolls"].botch, True)
-    #     self.assertEqual(cc.impossible, False)
-    #     self.assertEqual(
-    #         str(cc),
-    #         "@TestUser \n\n"
-    #         "Runde 1: EEW  17  16  10  Würfel  20  20  20  FW10  -3  -4 -10=-7FP  Patzer!\n\n"
-    #         "Probe fehlgeschlagen nach 1 Runden (4h)",
-    #     )
+    @patch("random.randint", new_callable=MagicMock())
+    def test_end2end(self, mock_randint: MagicMock):
+        mock_randint.return_value = 13
+        cc = CumulativeCheck(
+            "!Sammelprobe 5x2 Tage 12 14 10 @ 3", MockAuthor("TestUser")
+        )
+        self.assertEqual(
+            str(cc),
+            "@TestUser \n"
+            "```py\n"
+            "Runde  1: EEW  12  14  10  Würfel  13  13  13  FW3  -1      -3=-1FP  Nicht bestanden\n"
+            "Runde  2: EEW  11  13   9  Würfel  13  13  13  FW3  -2      -4=-3FP  Nicht bestanden\n"
+            "Runde  3: EEW  10  12   8  Würfel  13  13  13  FW3  -3  -1  -5=-6FP  Nicht bestanden\n"
+            "Runde  4: EEW   9  11   7  Würfel  13  13  13  FW3  -4  -2  -6=-9FP  Nicht bestanden\n"
+            "Runde  5: EEW   8  10   6  Würfel  13  13  13  FW3  -5  -3  -7=-12FP  Nicht bestanden\n\n"
+            "Probe fehlgeschlagen nach 5 Runden (10Tage)\n"
+            "```",
+        )
+
+        mock_randint.return_value = 13
+        cc = CumulativeCheck(
+            "!Sammelprobe 5x2 Tage 12 14 10 @ 5", MockAuthor("TestUser")
+        )
+        self.assertEqual(
+            str(cc),
+            "@TestUser \n"
+            "```py\n"
+            "Runde  1: EEW  12  14  10  Würfel  13  13  13  FW5  -1      -3=1FP  QS1\n"
+            "Runde  2: EEW  12  14  10  Würfel  13  13  13  FW5  -1      -3=1FP  QS1\n"
+            "Runde  3: EEW  12  14  10  Würfel  13  13  13  FW5  -1      -3=1FP  QS1\n"
+            "Runde  4: EEW  12  14  10  Würfel  13  13  13  FW5  -1      -3=1FP  QS1\n"
+            "Runde  5: EEW  12  14  10  Würfel  13  13  13  FW5  -1      -3=1FP  QS1\n\n"
+            "Probe fehlgeschlagen nach 5 Runden (10Tage)\n"
+            "```",
+        )
+
+        mock_randint.return_value = 13
+        cc = CumulativeCheck(
+            "!Sammelprobe 6x2 Tage 12 14 10 @ 5", MockAuthor("TestUser")
+        )
+        self.assertEqual(
+            str(cc),
+            "@TestUser \n"
+            "```py\n"
+            "Runde  1: EEW  12  14  10  Würfel  13  13  13  FW5  -1      -3=1FP  QS1\n"
+            "Runde  2: EEW  12  14  10  Würfel  13  13  13  FW5  -1      -3=1FP  QS1\n"
+            "Runde  3: EEW  12  14  10  Würfel  13  13  13  FW5  -1      -3=1FP  QS1\n"
+            "Runde  4: EEW  12  14  10  Würfel  13  13  13  FW5  -1      -3=1FP  QS1\n"
+            "Runde  5: EEW  12  14  10  Würfel  13  13  13  FW5  -1      -3=1FP  QS1\n"
+            "Runde  6: EEW  12  14  10  Würfel  13  13  13  FW5  -1      -3=1FP  QS1\n\n"
+            "Teilerfolg nach 6 Runden (12Tage)\n"
+            "```",
+        )
+
+        mock_randint.return_value = 13
+        cc = CumulativeCheck(
+            "!Sammelprobe 5x2 Tage 12 14 10 @ 10", MockAuthor("TestUser")
+        )
+        self.assertEqual(
+            str(cc),
+            "@TestUser \n"
+            "```py\n"
+            "Runde  1: EEW  12  14  10  Würfel  13  13  13  FW10  -1      -3=6FP  QS2\n"
+            "Runde  2: EEW  12  14  10  Würfel  13  13  13  FW10  -1      -3=6FP  QS2\n"
+            "Runde  3: EEW  12  14  10  Würfel  13  13  13  FW10  -1      -3=6FP  QS2\n"
+            "Runde  4: EEW  12  14  10  Würfel  13  13  13  FW10  -1      -3=6FP  QS2\n"
+            "Runde  5: EEW  12  14  10  Würfel  13  13  13  FW10  -1      -3=6FP  QS2\n\n"
+            "Probe erfolgreich nach 5 Runden (10Tage)\n"
+            "```",
+        )
+
+        mock_randint.return_value = 13
+        cc = CumulativeCheck(
+            "!Sammelprobe 5x2 Tage 12 14 10 @ 11", MockAuthor("TestUser")
+        )
+        self.assertEqual(
+            str(cc),
+            "@TestUser \n"
+            "```py\n"
+            "Runde  1: EEW  12  14  10  Würfel  13  13  13  FW11  -1      -3=7FP  QS3\n"
+            "Runde  2: EEW  12  14  10  Würfel  13  13  13  FW11  -1      -3=7FP  QS3\n"
+            "Runde  3: EEW  12  14  10  Würfel  13  13  13  FW11  -1      -3=7FP  QS3\n"
+            "Runde  4: EEW  12  14  10  Würfel  13  13  13  FW11  -1      -3=7FP  QS3\n\n"
+            "Probe erfolgreich nach 4 Runden (8Tage)\n"
+            "```",
+        )
 
     @patch("random.randint", new_callable=MagicMock())
     def test_end2end_crit(self, mock_randint: MagicMock):
