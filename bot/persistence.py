e32a958b73371c3e3ad830769f96942deddb679c
Florian Richter <florian.richter.de@gmail.com>
2020-08-21 13:10:56 +0200

    Use cursor and connection

--- bot/persistence.py
+++ bot/persistence.py
@@ -1,65 +1,74 @@
 import os
+import sqlite3
 from typing import List, Tuple
 
+import psycopg2
 from discord import TextChannel
 
 DB_PATH = os.getenv("DATABASE_URL")
-print(DB_PATH)
-connection = None
 
-if DB_PATH:
-    import psycopg2
 
-    print("Connecting to PostgreSQL!")
-    connection = psycopg2.connect(DB_PATH, sslmode="require")
-else:
-    import sqlite3
+def conn():
+    if DB_PATH:
+        return psycopg2.connect(DB_PATH, sslmode="require")
+    else:
+        return sqlite3.connect("bot/persistence.db")
 
-    print("Connecting to SQLite!")
-    connection = sqlite3.connect("bot/persistence.db")
 
 SCHEMA_PATH = "bot/schema.sql"
 
 
 def init_db() -> bool:
-    with open(SCHEMA_PATH, "r") as schemafile:
-        commands = schemafile.read()
-        connection.executescript(commands)
-        connection.commit()
+    with conn() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(open(SCHEMA_PATH).read())
+            connection.commit()
     return True
 
 
 def persist_channel(channel: TextChannel) -> bool:
-    connection.execute("INSERT OR REPLACE INTO channels VALUES (?)", (str(channel.id),))
-    connection.commit()
+    with conn() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "INSERT OR REPLACE INTO channels VALUES (?)", (str(channel.id),)
+            )
+            connection.commit()
     return True
 
 
 def remove_channel(channel: TextChannel) -> bool:
-    connection.execute("DELETE FROM channels WHERE id=?", (str(channel.id),))
-    connection.commit()
+    with conn() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("DELETE FROM channels WHERE id=?", (str(channel.id),))
+            connection.commit()
     return True
 
 
 def load_channels() -> List[Tuple[int]]:
-    results = connection.execute("SELECT * FROM channels").fetchall()
-    return results
+    with conn() as connection:
+        with connection.cursor() as cursor:
+            return cursor.execute("SELECT * FROM channels").fetchall()
 
 
 def persist_note(note_id: str, value: int) -> bool:
-    connection.execute(
-        "INSERT OR REPLACE INTO numberNotes VALUES (?, ?)", (note_id, value)
-    )
-    connection.commit()
+    with conn() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "INSERT OR REPLACE INTO numberNotes VALUES (?, ?)", (note_id, value)
+            )
+            connection.commit()
     return True
 
 
 def remove_note(note_id: str) -> bool:
-    connection.execute("DELETE FROM numberNotes WHERE id=?", (note_id,))
-    connection.commit()
+    with conn() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("DELETE FROM numberNotes WHERE id=?", (note_id,))
+            connection.commit()
     return True
 
 
 def load_notes() -> List[Tuple[str, int]]:
-    results = connection.execute("SELECT * FROM numberNotes").fetchall()
-    return results
+    with conn() as connection:
+        with connection.cursor() as cursor:
+            return cursor.execute("SELECT * FROM numberNotes").fetchall()
