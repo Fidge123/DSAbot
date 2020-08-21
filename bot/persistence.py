2bdc8f196e5cb1d1cfd4a6616fbab85bff318d88
Florian Richter <florian.richter.de@gmail.com>
2020-08-21 11:44:23 +0200

    Use postgres if running in the cloud

--- bot/persistence.py
+++ bot/persistence.py
@@ -1,67 +1,62 @@
-import sqlite3
+import os
 from typing import List, Tuple
 
 from discord import TextChannel
 
-DB_PATH = "bot/persistence.db"
+DB_PATH = os.getenv("DATABASE_URL")
+connection = None
+
+if DB_PATH:
+    import psycopg2
+
+    connection = psycopg2.connect(DB_PATH, sslmode="require")
+else:
+    import sqlite3
+
+    connection = sqlite3.connect("bot/persistence.db")
+
 SCHEMA_PATH = "bot/schema.sql"
 
 
 def init_db() -> bool:
     with open(SCHEMA_PATH, "r") as schemafile:
         commands = schemafile.read()
-
-    with sqlite3.connect(DB_PATH) as connection:
         connection.executescript(commands)
         connection.commit()
-
     return True
 
 
 def persist_channel(channel: TextChannel) -> bool:
-    with sqlite3.connect(DB_PATH) as connection:
-        connection.execute(
-            "INSERT OR REPLACE INTO channels VALUES (?)", (str(channel.id),)
-        )
-        connection.commit()
-
+    connection.execute("INSERT OR REPLACE INTO channels VALUES (?)", (str(channel.id),))
+    connection.commit()
     return True
 
 
 def remove_channel(channel: TextChannel) -> bool:
-    with sqlite3.connect(DB_PATH) as connection:
-        connection.execute("DELETE FROM channels WHERE id=?", (str(channel.id),))
-        connection.commit()
-
+    connection.execute("DELETE FROM channels WHERE id=?", (str(channel.id),))
+    connection.commit()
     return True
 
 
 def load_channels() -> List[Tuple[int]]:
-    with sqlite3.connect(DB_PATH) as connection:
-        results = connection.execute("SELECT * FROM channels").fetchall()
-
+    results = connection.execute("SELECT * FROM channels").fetchall()
     return results
 
 
 def persist_note(note_id: str, value: int) -> bool:
-    with sqlite3.connect(DB_PATH) as connection:
-        connection.execute(
-            "INSERT OR REPLACE INTO numberNotes VALUES (?, ?)", (note_id, value)
-        )
-        connection.commit()
-
+    connection.execute(
+        "INSERT OR REPLACE INTO numberNotes VALUES (?, ?)", (note_id, value)
+    )
+    connection.commit()
     return True
 
 
 def remove_note(note_id: str) -> bool:
-    with sqlite3.connect(DB_PATH) as connection:
-        connection.execute("DELETE FROM numberNotes WHERE id=?", (note_id,))
-        connection.commit()
+    connection.execute("DELETE FROM numberNotes WHERE id=?", (note_id,))
+    connection.commit()
     return True
 
 
 def load_notes() -> List[Tuple[str, int]]:
-    with sqlite3.connect(DB_PATH) as connection:
-        results = connection.execute("SELECT * FROM numberNotes").fetchall()
-
+    results = connection.execute("SELECT * FROM numberNotes").fetchall()
     return results
