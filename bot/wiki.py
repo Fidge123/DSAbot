c0cb38db3255365baa02dac29e82cd86416029b5
Florian Richter <florian.richter.de@gmail.com>
2020-08-26 17:31:37 +0200

    Calculate relevance for body if title has no good match

--- bot/wiki.py
+++ bot/wiki.py
@@ -10,20 +10,53 @@ from bot.response import Response
 DB_URL = os.getenv("HEROKU_POSTGRESQL_COBALT_URL")
 
 
-def next(user: Member, hits: List[Any]):
+def next(user: Member, hits: List[Any], search_term: str):
     yield user.mention
     yield from [
-        "{} ({}): <{}>".format(hit["title"], hit["score"], hit["url"]) for hit in hits
+        "{} ({}): <{}>".format(
+            hit["title"], _normalize(hit["score"], hit["body"], search_term), hit["url"]
+        )
+        for hit in hits
     ]
 
 
-def find(search_string: str) -> List[Any]:
+def _normalize(num: float, body: str, search_term: str) -> str:
+    if isinstance(num, int):
+        sections = body.split("\n\n")
+        num_contained = len([s for s in sections if search_term in s])
+        return f"{int((num_contained / len(sections)) * 100)}%"
+
+    else:
+        return f"{int(num * 100)}%"
+
+
+def find(search_string: str, in_body=False) -> List[Any]:
+    title_stmt = "SELECT title, url, body, word_similarity(%s, title) FROM regelwiki ORDER BY 4 DESC LIMIT 5"
+
+    body_stmt = """
+        SELECT *
+        FROM
+            (SELECT
+                title,
+                url,
+                body,
+                (length(body) - length(replace(body, %s, ''))) / length(%s) AS o,
+                parents
+            FROM regelwiki
+            ORDER BY 4 DESC
+            LIMIT 20) AS i
+        WHERE o > 0
+        ORDER BY array_length(i.parents, 1), o DESC
+        LIMIT 5
+        """
+
     with psycopg2.connect(DB_URL) as conn:
         with conn.cursor() as cur:
-            cur.execute(
-                "SELECT title, url, body, word_similarity(%s, title) FROM regelwiki ORDER BY 4 DESC LIMIT 5",
-                (search_string,),
-            )
+            if in_body:
+                cur.execute(body_stmt, (search_string, search_string))
+            else:
+                cur.execute(title_stmt, (search_string,))
+
             return [
                 {
                     "title": result[0],
@@ -35,19 +68,38 @@ def find(search_string: str) -> List[Any]:
             ]
 
 
+def filter_hits(hits: List[Any]) -> List[Any]:
+    return [hit for hit in hits if hit["score"] + 0.2 > hits[0]["score"]]
+
+
 def create_response(message: Message) -> Optional[Response]:
     match = re.search(r"^wiki\ (?P<search>.*)$", message.content, re.I)
     if match:
-        hits = find(match.group("search"))
-        response = Response(message.channel.send, "\n".join(next(message.author, hits)))
+        search_term = match.group("search")
+        title_match = filter_hits(find(search_term))
 
-        if hits[0]["score"] == 1 and hits[0]["body"]:
-            body = hits[0]["body"]
+        if title_match[0]["score"] < 0.6:
+            body_match = filter_hits(find(search_term, True))
+            return Response(
+                message.channel.send,
+                "\n".join(next(message.author, body_match, search_term)),
+            )
+
+        response = Response(
+            message.channel.send, "\n".join(next(message.author, title_match))
+        )
+
+        if title_match[0]["score"] == 1 and title_match[0]["body"]:
+            body = title_match[0]["body"]
+            next_message = ""
             for section in body.split("\n\n"):
-                if len(section) < 1024:
-                    response.append(message.author.send, section)
+                if len(next_message) + len(section) <= 2000:
+                    next_message = "\n\n".join([next_message, section])
                 else:
-                    response.append(message.author.send, section[:1020] + "...")
+                    response.append(message.author.send, next_message[:2000])
+                    next_message = section
+            if next_message:
+                response.append(message.author.send, next_message)
         return response
 
     return None
--- test/test_wiki.py
+++ test/test_wiki.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from bot import wiki
+from test.mocks import MockAuthor, MockMessage
 
 
 class TestWiki(TestCase):
@@ -27,3 +28,17 @@ class TestWiki(TestCase):
 
         self.assertEqual(results[0]["title"], "Wuchtschlag I-III")
         self.assertEqual(results[0]["score"], 1.0)
+
+        results = wiki.find("Passierschlag", True)
+
+        self.assertEqual(
+            results[0]["title"], "Kritische Erfolge bei Verteidigung im Nahkampf"
+        )
+        self.assertEqual(results[0]["score"], 37)
+
+    def test_res(self):
+        result = wiki.create_response(
+            MockMessage(MockAuthor("TestUser"), "wiki Passierschlag")
+        )
+        expected = "@TestUser\nKritische Erfolge bei Verteidigung im Nahkampf (46%)"
+        self.assertEqual(result.messages[0]["args"][0][: len(expected)], expected)
