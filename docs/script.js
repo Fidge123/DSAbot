b19077075d0f9f2b050b0fbbab4711c4831c3fff
Florian Richter <florian.richter.de@gmail.com>
2020-11-16 16:42:33 +0100

    Minor website improvements

--- docs/script.js
+++ docs/script.js
@@ -6,7 +6,7 @@ document.getElementById('proben-select').addEventListener('change', (ev) => {
   currentSection = ev.target.value;
 });
 
-for (let input of document.querySelectorAll("#Talentprobe > div > input")) {
+for (let input of document.querySelectorAll("#Talentprobe input")) {
   input.addEventListener("change", () => {
     const force = document.getElementById('talentprobe-r').checked ? 'force ' : '';
 
@@ -14,31 +14,34 @@ for (let input of document.querySelectorAll("#Talentprobe > div > input")) {
     const e2 = document.getElementById('talentprobe-e2').value;
     const e3 = document.getElementById('talentprobe-e3').value;
     const fw = document.getElementById('talentprobe-fw').value;
-    const mod = parseInt(document.getElementById('talentprobe-mod').value) || '';
+    const mod = modToString(document.getElementById('talentprobe-mod').value);
     const comment = document.getElementById('talentprobe-kommentar').value;
 
-    document.getElementById('talentprobe-output').innerText = `!${force}${e1},${e2},${e3}@${fw}${mod} ${comment}`
+    document.getElementById('talentprobe-output').innerText = `!${force}${e1},${e2},${e3} @ ${fw} ${mod} ${comment}`
   });
 }
 
-for (let input of document.querySelectorAll("#Sammelprobe > div > input")) {
-  input.addEventListener("change", () => {
-    const force = document.getElementById('sammelprobe-r').checked ? 'force ' : '';
+const updateSammelprobe = () => {
+  const force = document.getElementById('sammelprobe-r').checked ? 'force ' : '';
 
-    const v = document.getElementById('sammelprobe-versuche').value;
-    const time = document.getElementById('sammelprobe-zw').value + document.getElementById('sammelprobe-ze').value;
-    const e1 = document.getElementById('sammelprobe-e1').value;
-    const e2 = document.getElementById('sammelprobe-e2').value;
-    const e3 = document.getElementById('sammelprobe-e3').value;
-    const fw = document.getElementById('sammelprobe-fw').value;
-    const mod = parseInt(document.getElementById('sammelprobe-mod').value) || '';
-    const comment = document.getElementById('sammelprobe-kommentar').value;
+  const v = document.getElementById('sammelprobe-versuche').value;
+  const time = document.getElementById('sammelprobe-zw').value + document.getElementById('sammelprobe-ze').value;
+  const e1 = document.getElementById('sammelprobe-e1').value;
+  const e2 = document.getElementById('sammelprobe-e2').value;
+  const e3 = document.getElementById('sammelprobe-e3').value;
+  const fw = document.getElementById('sammelprobe-fw').value;
+  const mod = modToString(document.getElementById('sammelprobe-mod').value);
+  const comment = document.getElementById('sammelprobe-kommentar').value;
 
-    document.getElementById('sammelprobe-output').innerText = `!${force}S${v}x${time} ${e1},${e2},${e3}@${fw}${mod} ${comment}`
-  });
+  document.getElementById('sammelprobe-output').innerText = `!${force}S${v}x${time} ${e1},${e2},${e3} @ ${fw} ${mod} ${comment}`
+};
+
+for (let input of document.querySelectorAll("#Sammelprobe input")) {
+  input.addEventListener("change", updateSammelprobe);
 }
+document.getElementById('sammelprobe-ze').addEventListener('change', updateSammelprobe);
 
-for (let input of document.querySelectorAll("#Schips > div > input")) {
+for (let input of document.querySelectorAll("#Schips input")) {
   input.addEventListener("change", () => {
     const dice1 = document.getElementById('schip-1').checked ? 'r' : 'k';
     const dice2 = document.getElementById('schip-2').checked ? 'r' : 'k';
@@ -57,13 +60,13 @@ document.getElementById('repeat-r').addEventListener(
   ev => document.getElementById('repeat-output').innerText = ev.target.checked ? 'retry' : "repeat"
 );
 
-for (let input of document.querySelectorAll("#Notiz > div > input")) {
+for (let input of document.querySelectorAll("#Notiz input")) {
   input.addEventListener("change", () => {
     if (document.getElementById('notiz-delete').checked) {
-      document.getElementById('notiz-output').innerText = `delete note ${document.getElementById('notiz-id').value}`;
+      document.getElementById('notiz-output').innerText = `delete note ${sanitizeID(document.getElementById('notiz-id').value)}`;
     } else {
       document.getElementById('notiz-output').innerText =
-        `note:${document.getElementById('notiz-id').value}->${document.getElementById('notiz-value').value}`;
+        `note:${sanitizeID(document.getElementById('notiz-id').value)}->${document.getElementById('notiz-value').value}`;
     }
   });
 }
@@ -72,3 +75,17 @@ document.getElementById('wiki-search').addEventListener(
   'change',
   ev => document.getElementById('wiki-output').innerText = `wiki ${ev.target.value}`
 );
+
+function sanitizeID(noteID) {
+  return noteID.replaceAll(' ', '_').toLowerCase();
+}
+
+function modToString(number) {
+  if (number > 0) {
+    return `+${number.toString()}`;
+  }
+  if (number < 0) {
+    return number.toString();
+  }
+  return '';
+}
