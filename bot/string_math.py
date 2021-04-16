2523c9aa9bc14c63d750bcdef4d47e40109524f5
Florian Richter <flori@hey.com>
2021-04-16 10:52:09 +0200

    Support complex dice math

--- bot/string_math.py
+++ bot/string_math.py
@@ -1,4 +1,5 @@
 import operator
+import random
 from pyparsing import (
     ParserElement,
     pyparsing_common as ppc,
@@ -8,6 +9,14 @@ from pyparsing import (
 )
 
 ParserElement.enablePackrat()
+
+
+def dice_roll(amount: int, sides: int) -> int:
+    aggregate = 0
+    for _ in range(int(amount)):
+        roll = random.randint(1, int(sides))
+        aggregate += roll
+    return aggregate
 
 
 class EvalBase:
@@ -27,6 +36,24 @@ class EvalSignOp:
     def eval(self) -> float:
         mult = {"+": 1, "-": -1}[self.sign]
         return mult * self.value.eval()
+
+
+class EvalDieOp:
+    def __init__(self, tokens):
+        self.sign, self.value = tokens[0]
+
+    def eval(self) -> int:
+        return dice_roll(1, self.value.eval())
+
+
+class EvalDiceOp(EvalBase):
+    def eval(self) -> int:
+        res = self.value[0].eval()
+
+        it = iter(self.value[1:])
+        for op, val in zip(it, it):
+            res = dice_roll(res, val.eval())
+        return res
 
 
 class EvalInfixOp(EvalBase):
@@ -49,7 +76,9 @@ class EvalInfixOp(EvalBase):
 grammar = infixNotation(
     ppc.integer.setParseAction(EvalConstant),
     [
+        (oneOf("d w D W"), 1, opAssoc.RIGHT, EvalDieOp),
         (oneOf("+ -"), 1, opAssoc.RIGHT, EvalSignOp),
+        (oneOf("d w D W"), 2, opAssoc.LEFT, EvalDiceOp),
         (oneOf("* /"), 2, opAssoc.LEFT, EvalInfixOp),
         (oneOf("+ -"), 2, opAssoc.LEFT, EvalInfixOp),
     ],
--- test/test_string_math.py
+++ test/test_string_math.py
@@ -1,13 +1,30 @@
 from unittest import TestCase
+from unittest.mock import MagicMock, patch
 
-from bot import string_math
+from bot.string_math import calc
 
 
 class TestStringMath(TestCase):
     def test_calc(self):
-        self.assertEqual(string_math.calc("1"), 1)
-        self.assertEqual(string_math.calc("1+1"), 2)
-        self.assertEqual(string_math.calc("1-1"), 0)
-        self.assertEqual(string_math.calc("-1"), -1)
-        self.assertEqual(string_math.calc("(2 + 1 * 2) / 2"), 2)
-        self.assertEqual(string_math.calc("+ ((4 + -1 * 2) / 2) * (1+-3/4) "), 0.25)
+        self.assertEqual(calc("1"), 1)
+        self.assertEqual(calc("1+1"), 2)
+        self.assertEqual(calc("1-1"), 0)
+        self.assertEqual(calc("-1"), -1)
+        self.assertEqual(calc("(2 + 1 * 2) / 2"), 2)
+        self.assertEqual(calc("+ ((4 + -1 * 2) / 2) * (1+-3/4) "), 0.25)
+
+    @patch("random.randint", new_callable=MagicMock())
+    def test_with_dice(self, mock_randint: MagicMock):
+        mock_randint.return_value = 1
+
+        self.assertEqual(calc("1d6"), 1)
+        self.assertEqual(calc("14+1d6"), 15)
+        self.assertEqual(calc("5+1w6+2"), 8)
+        self.assertEqual(calc("3w20 + 1 - 4"), 0)
+        self.assertEqual(calc("1337d1337"), 1337)
+        self.assertEqual(calc("4W20"), 4)
+        self.assertEqual(calc("w3"), 1)
+        self.assertEqual(calc("-w6 "), -1)
+
+        mock_randint.return_value = 2
+        self.assertEqual(calc("(3*2w6+1)d(1W6+2)"), 26)
